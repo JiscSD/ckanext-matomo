19402e6b898c844e5425c86108b9c2d99d2d7b9f
Chris Daly <chris.daly@jisc.ac.uk>
2022-10-10 15:29:11 +0100

    Added parts for package upload
    Made the setup script compliant with PyPi

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
 
     description='''Matomo analytics for CKAN''',
     long_description=long_description,
+    long_description_content_type='text/x-rst',
 
     # The project's main homepage.
     url='https://github.com/vrk-kpa/ckanext-matomo',
@@ -44,6 +45,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 
 
